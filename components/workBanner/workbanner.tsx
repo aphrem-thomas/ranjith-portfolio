3a3d35a47c6dd02addb2fc146593edafed4d793c
aphremthomas <aphremthomas@gmail.com>
2023-08-04 12:47:49 -0400

    banner removed

--- components/workBanner/workbanner.tsx
+++ components/workBanner/workbanner.tsx
@@ -11,34 +11,8 @@ const anton = Anton({
 function WorkBanner () {
     // const [activeTab, setActiveTab] = useState('home')
     return(
-        <div className="banner w-screen mt-40 bg-text p-5 flex flex-col h-full items-center">
-            <h3 className="text-background mb-5 text-3xl">I am a member of</h3>
-            <div className="worklist flex flex-wrap justify-around container">
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale"src="iccc.png"/>
-                </div>
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale" src="jci.webp"/>
-                </div>
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale" src="obot.png"/>
-                </div>
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale" src="ociso.svg"/>
-                </div>
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale" src="tekno.png"/>
-                </div>
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale" src="EOTB.webp"/>
-                </div>
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale" src="tie.png"/>
-                </div>
-                <div className="logo">
-                    <img className="w-40 h-14 object-contains grayscale" src="World-Skills.png"/>
-                </div>
-            </div>
+        <div className="banner w-screen mt-40 bg-accent p-5 flex flex-col h-full items-center">
+            
         </div>
     )
 }
