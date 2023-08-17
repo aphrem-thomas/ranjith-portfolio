646deffee17edc58ff853f1fbfd606498a8da514
aphremthomas <aphremthomas@gmail.com>
2023-08-17 10:24:16 -0400

    middleware corrected

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,23 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { connect } from './app/config/db.config'
 import User from './app/model/user.model'
-import jwt from 'jsonwebtoken'
+import * as jose from 'jose'
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-    console.log("in middleware--->")
-    // let token=request.cookies.get('token')?.value;
-    // if(token){
-    //   let udt = jwt.verify(token,process.env.SIGN_HASH!)
-    //   console.log("udt", udt)
-    // }
-    // connect()
-    // let oup=User.findOne({username:'aphremthomas'})
-    // console.log('out',oup)
-  // return NextResponse.redirect(new URL('/home', request.url))
+    let token=request.cookies.get('token')?.value;
+    if(token){
+      try {
+        let udt = await jose.jwtVerify(token, new TextEncoder().encode(process.env.SIGN_HASH!))
+        console.log("udt", udt)
+        if (udt) {
+          return NextResponse.redirect(new URL('/', request.url))
+        }
+      } catch (e) {
+        return NextResponse.redirect(new URL('/login', request.url))
+      }
+    }
+    
 }
  
 export const config = {
