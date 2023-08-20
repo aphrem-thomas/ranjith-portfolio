b035ce0f9dcfceff97ac4904ebb1ad2772309ae2
aphremthomas <aphremthomas@gmail.com>
2023-08-20 15:42:44 -0400

    auth function done

--- app/helper/authenticate.ts
+++ app/helper/authenticate.ts
@@ -4,26 +4,14 @@ import jwt from 'jsonwebtoken'
 import User from "../model/user.model";
 
 export async function authenticate(request:NextRequest){
-    connect()
     try{
     let token=request.cookies.get('token')?.value;
     if(token){
       let udt = await jwt.verify(token,process.env.SIGN_HASH!)
       console.log(typeof(udt))
       if(udt){
-        let usr = await User.findOne({username:JSON.parse(JSON.stringify(udt))?.user})
-        if(usr) {
-            if(usr?.verifyToken ===token){
-                console.log("token verified");
-                return true
-            }
-            else {
-                return false
-            }
-        } else {
-            return false
-        }
-      }else {
+        return true
+      }else{
         return false
       }
     }
