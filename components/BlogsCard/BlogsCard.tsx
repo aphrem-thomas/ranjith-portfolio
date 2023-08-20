f3f2048545b9019f98d9e4dedd143777c2180375
aphremthomas <aphremthomas@gmail.com>
2023-08-20 15:32:31 -0400

    blogs card created

---
+++ components/BlogsCard/BlogsCard.tsx
@@ -0,0 +1,15 @@
+const BlogsCard = (props)=>{
+    return(
+        <div className="blogsCardmain">
+            <div className="descriptionbox">
+                <div className="authorName"></div>
+                <h1 className="header"></h1>
+                <h2 className="subhead"></h2>
+                <div className="meta">
+                    
+                </div>
+            </div>
+            <div className="thumbnailImage"></div>
+        </div>
+    )
+}
