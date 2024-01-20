da50962427db1d5c09fe63676120f4571a23c0aa
aphremthomas <aphremthomas@gmail.com>
2024-01-20 14:35:04 -0500

    down arrow added

--- app/page.tsx
+++ app/page.tsx
@@ -12,41 +12,47 @@ const anton = Anton({
 
 export default function Home() {
   return (
-    <div className={`parent flex w-full flex-col items-center`}>
-      <div className="container flex">
+   <div className={`parent scroll-smooth flex w-full flex-col items-center`}>
+      <div className="container relative flex flex-col h-[calc(100vh-7rem)]">
         <div className="picAndText flex items-center w-full flex-col">
           <div className="hello text-8xl mb-10 flex flex-col md:hidden">
-              <div className={`text-text ${anton.className} tracking-[13px]`}>RANJITH</div>
-              <div className={`text-text ${anton.className} tracking-[7px]`}>MATHEW</div>
+            <div className={`text-text ${anton.className} tracking-[13px]`}>
+              RANJITH
             </div>
-          <div className="dpimage lg:h-[35rem] 2xl:h-[46rem] h-full flex flex-row md:flex-col md:justify-center md:relative md:mt-12">
+            <div className={`text-text ${anton.className} tracking-[7px]`}>
+              MATHEW
+            </div>
+          </div>
+          <div className="dpimage h-full flex flex-row md:flex-col md:justify-center md:relative md:mt-12 lg:mt-2 lg:h-[30rem] 2xl:h-[46rem]">
             <img
               src="/Ranjith.jpg"
               alt="pic"
               className="w-auto h-auto max-w-full max-h-full"
             />
-            <div className="hidden md:flex hello lg:left-[19rem] 2xl:left-[26rem] lg:top-20  2xl:top-40 absolute lg:text-4xl 2xl:text-6xl bg-text h-24 lg:w-52 2xl:w-80 mb-10 flex-col justify-center items-center">
+            <div className="hidden md:flex hello lg:left-[15rem] 2xl:left-[26rem] lg:top-20  2xl:top-40 absolute lg:text-4xl 2xl:text-6xl bg-text h-24 lg:w-52 2xl:w-80 mb-10 flex-col justify-center items-center">
               <p className="text-background ">Hi it&apos;s Ranjith...</p>
             </div>
           </div>
-          <div className="intro mt-2 md:mt-10 flex flex-col justify-center items-center relative">
+          <div className="intro mt-2 md:mt-2 flex flex-col justify-center items-center relative">
             <div className="statement p-4 md:w-2/3 text-xl text-justify">
               <p>
-                Ranjith Mathew is a passionate community builder,
-                skilled in fostering connections and a talented career
-                management professional with expertise in job
-                development, resume writing, and career mentorship
-                for individuals including new immigrants, recent
-                graduates, and job seekers. He actively engages in
-                community development and youth leadership
-                initiatives and serves as a board member for several
-                youth-focused nonprofit organizations in Canada.
+                Ranjith Mathew is a passionate community builder, skilled in
+                fostering connections and a talented career management
+                professional with expertise in job development, resume writing,
+                and career mentorship for individuals including new immigrants,
+                recent graduates, and job seekers. He actively engages in
+                community development and youth leadership initiatives and
+                serves as a board member for several youth-focused nonprofit
+                organizations in Canada.
               </p>
             </div>
           </div>
         </div>
+        <span className="absolute bottom-0 hidden md:flex h-10 w-10 animate-bounce">
+           <a href="#work_banner"><img src="/arrow.png" className="rotate-90 h-10 w-10 rounded-full text-background text-center text-4xl"/></a> 
+        </span>
       </div>
       <WorkBanner/>
     </div>
-  )
+  );
 }
--- components/workBanner/workbanner.tsx
+++ components/workBanner/workbanner.tsx
@@ -1,53 +1,127 @@
+"use client";
+
 // import { useState } from "react";
-import {Anton} from 'next/font/google'
-import Link from "next/link";
+import { Anton } from "next/font/google";
 
+function getScreenType(): string {
+  const resolution = window.innerWidth;
+  if (resolution > 1536) return "2xl";
+  else if (resolution > 1280) return "xl";
+  else if (resolution > 1024) return "lg";
+  else if (resolution > 768) return "md";
+  else if (resolution > 640) return "sm";
+  return "";
+}
 const anton = Anton({
-    subsets: ['latin'],
-    weight: '400',
-    variable:'--anton-font'
-  })
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--anton-font",
+});
 
-function WorkBanner () {
-    // const [activeTab, setActiveTab] = useState('home')
-    return(
-        <div className="banner w-full overflow-y-scroll mt-40 bg-accent p-5 flex  h-full ">
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6469682871002107904" height="453" title="Embedded post"></iframe> 
-            </div>
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7005674165185785857" height="453" title="Embedded post"></iframe>
-            </div>
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6988890725035753472" height="453" title="Embedded post"></iframe>
-            </div>
+function getContentWidth(type: string) {
+  switch (type) {
+    case "sm":
+      return "250";
+    case "md":
+      return "600";
+    case "lg":
+      return "600";
+    case "xl":
+      return "600";
+    case "2xl":
+      return "600";
+  }
+}
+const screenSize = getContentWidth(getScreenType());
 
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6973102261698752512" height="453" title="Embedded post"></iframe>
-            </div>
+function WorkBanner() {
+  return (
+    <div  id="work_banner" className="workbanner w-full mt-96 md:mt-0 bg-background-2">
+        <div className="heading flex justify-center mt-10 mb-4 text-4xl md:text-8xl">
+            Notable works
+        </div>
+      <div className="banner w-full overflow-y-scroll p-5 flex h-full md:flex md:flex-wrap md:overflow-hidden md:justify-around ">
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6469682871002107904"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:7005674165185785857"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6988890725035753472"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
 
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6967620616208142336" height="453" title="Embedded post"></iframe>
-            </div>
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6973102261698752512"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
 
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6951612774665904128" height="453" title="Embedded post"></iframe>
-            </div>
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6967620616208142336"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
 
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6949716820765392897" height="453" title="Embedded post"></iframe>
-            </div>
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6951612774665904128"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
 
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6947989831419392000" height="453" title="Embedded post"></iframe>
-            </div>
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6949716820765392897"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
 
-            <div className="bannerItem mt-12 p-4">
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6947576278577270784" height="453" title="Embedded post"></iframe>
-            </div>
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6947989831419392000"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
 
-            </div>
-    )
+        <div className="bannerItem mt-12 p-4">
+          <iframe
+            src="https://www.linkedin.com/embed/feed/update/urn:li:share:6947576278577270784"
+            height="453"
+            width={screenSize}
+            title="Embedded post"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WorkBanner;
