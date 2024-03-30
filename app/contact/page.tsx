29667eecad16d2246c705c2be41eaeede03915a0
aphremthomas <aphremthomas@gmail.com>
2024-03-30 16:32:16 -0400

    profile pic changed and intro updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -51,8 +51,7 @@ function Contacts() {
                 <h1 className="text-2xl md:text-6xl font-bold">Free Resume review</h1>
                 <div className="disclaimerMessage mt-5 leading-6">
                 <p>As the Technical Resource Manager, my primary responsibilities encompass generating fresh sales leads, overseeing account management, and adeptly identifying outstanding technical talents, particularly for software and hardware development enterprises throughout North America. Leveraging my remarkable networking skills and talent for fostering relationships, I excel at engaging with clients and aiding them in forming high-achieving teams. </p>
-                <p>Beyond my professional endeavors, I derive immense satisfaction from forging community ties and offering invaluable career guidance to newcomers in Canada. My profound dedication to nurturing skills in the leaders of tomorrow drives me to actively participate in volunteer work for non-profit organizations and educational institutions.</p>
-                <p>When I seek moments of serenity and a deeper connection with the natural world, I find solace in the art of fishing. Whether it involves casting a line in a nearby river or embarking on an excursion to a tranquil fishing spot, fishing serves as a means of relaxation and reconnection for me.</p></div>
+            </div>
             </div>
             <div className="contactForm md:container md:flex md:justify-center md:ml-10">
                 <div className="md:w-[30rem]">
--- app/page.tsx
+++ app/page.tsx
@@ -27,21 +27,23 @@ export default function Home() {
           <div className="intro mt-2 md:mt-2 flex flex-col-reverse md:flex-row justify-center items-center relative">
             <div className="statement flex-col p-4 md:w-2/3 text-xl text-justify">
               <div>Hi it,s</div>
-              <div className="text-5xl md:text-9xl mt-2">Ranjith Mathew</div>
-              <p className="md:text-xl mt-7 animate-fade-up animate-infinite">
+              <div className="text-4xl md:text-6xl mt-2">Ranjith Mathew</div>
+              <p className="md:text-lg mt-7 animate-fade-up animate-infinite">
                 I am a passionate community builder, skilled in
                 fostering connections and a talented career management
                 professional with expertise in job development, resume writing,
                 and career mentorship for individuals including new immigrants,
-                recent graduates, and job seekers. He actively engages in
+                recent graduates, and job seekers. I actively engages in
                 community development and youth leadership initiatives and
                 serves as a board member for several youth-focused nonprofit
                 organizations in Canada.
               </p>
+              <p className="md:text-lg">Beyond my professional endeavors, I derive immense satisfaction from forging community ties and offering invaluable career guidance to newcomers in Canada. My profound dedication to nurturing skills in the leaders of tomorrow drives me to actively participate in volunteer work for non-profit organizations and educational institutions.</p>
+              <p className="md:text-lg">When I seek moments of serenity and a deeper connection with the natural world, I find solace in the art of fishing. Whether it involves casting a line in a nearby river or embarking on an excursion to a tranquil fishing spot, fishing serves as a means of relaxation and reconnection for me.</p>
             </div>
             <div className="dpimage w-full md:w-auto flex flex-row-reverse mr-10 md:ml-4 md:flex-col md:justify-center md:relative md:mt-12 lg:mt-2 lg:h-[30rem]">
             <img
-              src="/Ranjith.jpg"
+              src="/rm-profile.jpg"
               alt="pic"
               className="rounded-full rounded-bl-none h-60 max-w-full md:rounded-none md:h-full"
             />
