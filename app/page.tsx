80829bf43cdcad40dcea6ed6caca0b7d1c41d43e
aphremthomas <aphremthomas@gmail.com>
2024-04-01 19:42:51 -0400

    lint corrected

--- app/page.tsx
+++ app/page.tsx
@@ -26,10 +26,10 @@ export default function Home() {
           
           <div className="intro mt-2 md:mt-2 flex flex-col-reverse md:flex-row justify-center items-center relative">
             <div className="statement flex-col p-4 md:w-2/3 text-xl text-justify">
-              <div>Hi it's</div>
+              <div>Hi it&apos;s</div>
               <div className="text-4xl md:text-6xl mt-2">Ranjith Mathew</div>
               <p className="md:text-lg mt-7 animate-fade-up animate-infinite">
-                A dedicated community builder and career management professional excels in fostering connections and providing expert guidance in job development, resume writing, and career mentorship. Their focus extends to supporting diverse individuals, including immigrants, graduates, and job seekers, through various initiatives. Active involvement in youth leadership programs and serving on nonprofit boards showcases a commitment to shaping the leaders of tomorrow. Outside of professional commitments, solace is found in fishing, whether casting a line in a local river or venturing to serene spots, allowing for a reconnection with nature and moments of tranquility amidst life's busyness.
+                A dedicated community builder and career management professional excels in fostering connections and providing expert guidance in job development, resume writing, and career mentorship. Their focus extends to supporting diverse individuals, including immigrants, graduates, and job seekers, through various initiatives. Active involvement in youth leadership programs and serving on nonprofit boards showcases a commitment to shaping the leaders of tomorrow. Outside of professional commitments, solace is found in fishing, whether casting a line in a local river or venturing to serene spots, allowing for a reconnection with nature and moments of tranquility amidst life&apos; s busyness.
               </p>
             </div>
             <div className="dpimage w-full md:w-auto flex flex-row-reverse mr-10 md:ml-4 md:flex-col md:justify-center md:relative md:mt-12 lg:mt-2 lg:h-[30rem]">
