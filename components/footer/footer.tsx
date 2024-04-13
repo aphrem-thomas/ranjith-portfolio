f7cdc58ec37b964961bb2820d0460fd9555f7926
aphremthomas <aphremthomas@gmail.com>
2024-04-13 19:19:57 -0400

    updated links to social media

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,20 +4,23 @@ function Footer () {
    
     return(
         <>
-        <div className="footermain bg-text text-background-1 w-full p-2 flex justify-center h-32">
-            <div className="rmfootercontact flex items-center justify-between w-full md:max-w-5xl">
-                <div className="basiccontact">
-                    <div className="text-xm">Ranjith Mathew</div>
-                    <div className="text-xs">Director</div>
-                    <div className="text-xs">rm@gmail.com</div>
+        <div className="footermain bg-text text-background-1 w-full p-4 flex  justify-center min-h-32">
+            <div className="rmfootercontact flex flex-col items-center justify-center md:justify-between w-full md:max-w-5xl">
+                <div className='flex items-center w-full md:max-w-5xl'>
+                    <div className="basiccontact w-full">
+                        <div className="text-xm">Ranjith Mathew</div>
+                        <div className="text-xs">Director</div>
+                        <div className="text-xs">rm@gmail.com</div>
+                    </div>
+                    <div className="socialmedia ml-10 flex w-full justify-end">
+                        <a target="_blank" href='https://www.facebook.com/ranjithmathew.ca'><img className="h-8 ml-2" src="/icons/facebook.png"></img></a>
+                        <a target="_blank" href='https://www.linkedin.com/in/ranjithmathew/'><img className="h-8 ml-2" src="/icons/linkedin.png"></img></a>
+                        <a target="_blank" href='https://twitter.com/mathew_ranjith'><img className="h-8 ml-2" src="/icons/twitter.png"></img></a>
+                        <a target="_blank" href='https://www.youtube.com/@ranjithmathew.canada'><img className="h-8 ml-2" src="/icons/youtube.png"></img></a>
+                    </div>
                 </div>
-                <div className="socialmedia flex w-32 justify-between">
-                    <a href=''><img className="h-8" src="/icons/facebook.png"></img></a>
-                    <a href=''><img className="h-8" src="/icons/linkedin.png"></img></a>
-                    <a href=''><img className="h-8" src="/icons/twitter.png"></img></a>
-                </div>
-                <div className="sitedeveloper flex ">
-                    <div className="dev text-[3px] h-8 md:text-sm underline">Developed by, <a target="_blank" href="https://www.linkedin.com/in/aphremthomas/">Aphrem Thomas</a></div>
+                <div className="sitedeveloper flex mt-4 ">
+                    <div className="dev text-xs h-8 md:text-sm underline">Developed by, <a target="_blank" href="https://www.linkedin.com/in/aphremthomas/">Aphrem Thomas</a></div>
                 </div>
             </div>
         </div>
