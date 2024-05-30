55ff620351e7c6e1c7df640c9e713333576b31ee
aphremthomas <aphremthomas@gmail.com>
2024-05-30 17:48:00 -0400

    updated text

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,126 +1,204 @@
-'use client'
+"use client";
 
-import AlertMessage from "@/components/alertMessage/AlertMessage"
-import { useState } from "react"
-
+import AlertMessage from "@/components/alertMessage/AlertMessage";
+import { useState } from "react";
 
 function Contacts() {
+  const [message, setMessage] = useState("");
+  const [type, setType] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [resume, setResume] = useState<any>("");
+  const [description, setDescription] = useState<any>("");
+  const [alert, showAlert] = useState(false);
 
-    const [message, setMessage] = useState('')
-    const [type, setType] = useState('')
-    const [firstname, setFirstName] = useState('')
-    const [lastname, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [resume, setResume] = useState<any>('')
-    const [description, setDescription] = useState<any>('')
-    const [alert, showAlert] = useState(false)
-
-    async function handleSubmit(){
-        if(firstname ==='' || lastname==='' || email ==='' ||resume ==='' ||description===''){
-            setType('error')
-            setMessage('Please fill all fields')
-            showAlert(true)
-            return
-        }
-        const formData = new FormData()
-        console.log("data", firstname, lastname, email, resume, description)
-        formData.append("name", firstname+' '+lastname)
-        formData.append('email',email)
-        formData.append('resume', resume)
-        formData.append('description', description)
-        let resp = await fetch('api/contact',{
-            method:'POST',
-            body:formData,
-            cache: "no-cache",
-            mode: "no-cors",
-        })
-        if(resp.ok){
-            setType('success')
-            setMessage('Successfully submitted your data')
-            showAlert(true)
-            setEmail('')
-            setDescription('')
-            setFirstName('')
-            setLastName('')
-            setResume('')
-        }else{
-            setType('error')
-            setMessage('error in subimitting data')
-            showAlert(true)
-            setEmail('')
-            setDescription('')
-            setFirstName('')
-            setLastName('')
-            setResume('')  
-        }
+  async function handleSubmit() {
+    if (
+      firstname === "" ||
+      lastname === "" ||
+      email === "" ||
+      resume === "" ||
+      description === ""
+    ) {
+      setType("error");
+      setMessage("Please fill all fields");
+      showAlert(true);
+      return;
     }
-    return (
-        <>
-        <div className="contactsMain flex min-h-screen md:mt-20 md:content-centre md:max-w-5xl">
-            <div className="flex flex-col md:flex-row">
-            <div className="disclaimer p-10 md:p-0 md:max-w-5xl">
-                <div>Submit the form for</div>
-                <h1 className="text-2xl md:text-6xl font-bold">Free Resume review</h1>
-                <div className="disclaimerMessage mt-5 leading-6">
-                <p>In the role of Technical Resource Manager, primary responsibilities involve, overseeing account management, generating new sales leads and identifying top technical talent, especially for software and hardware development companies across North America. Leveraging exceptional networking abilities and relationship-building talent, excellence is achieved in client engagement and assisting them in building successful teams.</p>
-                <br/>
-                <p className="text font-bold">Disclaimer</p>
-                <p className="text text-sm italic">Please note that by submitting your resume for a free 15-minute consulting session, you consent to the collection and use of your personal information solely for the purpose of providing career guidance and employment assistance. Your information will be treated confidentially and will not be shared with any third parties without your explicit consent.</p>
+    const formData = new FormData();
+    console.log("data", firstname, lastname, email, resume, description);
+    formData.append("name", firstname + " " + lastname);
+    formData.append("email", email);
+    formData.append("resume", resume);
+    formData.append("description", description);
+    let resp = await fetch("api/contact", {
+      method: "POST",
+      body: formData,
+      cache: "no-cache",
+      mode: "no-cors",
+    });
+    if (resp.ok) {
+      setType("success");
+      setMessage("Successfully submitted your data");
+      showAlert(true);
+      setEmail("");
+      setDescription("");
+      setFirstName("");
+      setLastName("");
+      setResume("");
+    } else {
+      setType("error");
+      setMessage("error in subimitting data");
+      showAlert(true);
+      setEmail("");
+      setDescription("");
+      setFirstName("");
+      setLastName("");
+      setResume("");
+    }
+  }
+  return (
+    <>
+      <div className="contactsMain flex min-h-screen md:mt-20 md:content-centre md:max-w-5xl">
+        <div className="flex flex-col md:flex-row">
+          <div className="disclaimer p-10 md:p-0 md:max-w-5xl">
+            <div>Submit the form for</div>
+            <h1 className="text-2xl md:text-6xl font-bold">
+              Free Resume review
+            </h1>
+            <div className="disclaimerMessage mt-5 leading-6">
+              <p>
+                Throughout my career, I have assisted over 1,000 new
+                graduates and newcomers in Ottawa in finding job opportunities
+                in the business and IT security industries. My proficiency in
+                community outreach and stakeholder development enables him to
+                build new collaborations, driving solutions for nonprofits and
+                business leaders alike. Ranjith wishes to be your #networKING,
+                passionately connecting people in a world where uncompromised
+                digital trust is essential.
+                </p>
+                <p>
+                Iam currently a Technical
+                Resource Manager, my main duties include overseeing account
+                management, creating new hiring opportunities, and identifying
+                top technical talent, particularly for software and hardware
+                development companies across North America. By leveraging my
+                exceptional networking skills and talent for building
+                relationships, I excels in client engagement and helps clients
+                build successful teams.
+              </p>
+              <br />
+              <p className="text font-bold">Disclaimer</p>
+              <p className="text text-sm italic">
+                Please note that by submitting your resume for a free 15-minute
+                consulting session, you consent to the collection and use of
+                your personal information solely for the purpose of providing
+                career guidance and employment assistance. Your information will
+                be treated confidentially and will not be shared with any third
+                parties without your explicit consent.
+              </p>
             </div>
-            </div>
-            <div className="contactForm md:container md:flex md:justify-center md:ml-10">
-                <div className="md:w-[30rem]">
-                    <div className="bg-background-1 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                First name
-                            </label>
-                            <input value={firstname} onChange={(e)=>setFirstName(e.target.value)}className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="John"/>
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Last name
-                            </label>
-                            <input value={lastname} onChange={(e)=>setLastName(e.target.value)}className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Denver"/>
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Email
-                            </label>
-                            <input value={email} onChange={(e)=>setEmail(e.target.value)} className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="john@gmail.com"/>
-                                <p className="text-red-500 text-xs italic">Please enter email.</p>
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Message
-                            </label>
-                            <textarea value={description} onChange={(e)=>setDescription(e.target.value)} className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"/>
-                                <p className="text-red-500 text-xs italic">Enter description</p>
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Resume
-                            </label>
-                            <input onChange={(e)=>{
-                                if(e.target.files?.length){
-                                    setResume(e.target.files[0])
-                                }
-                            }} accept=".pdf"className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="resume" type="file"/>
-                                <p className="text-red-500 text-xs italic">Please attach resume in pdf.</p>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <button onClick={handleSubmit} className="bg-primary hover:bg-accent text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                Submit
-                            </button>
-                        </div>
-                    </div>
+          </div>
+          <div className="contactForm md:container md:flex md:justify-center md:ml-10">
+            <div className="md:w-[30rem]">
+              <div className="bg-background-1 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    First name
+                  </label>
+                  <input
+                    value={firstname}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    placeholder="John"
+                  />
                 </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Last name
+                  </label>
+                  <input
+                    value={lastname}
+                    onChange={(e) => setLastName(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    placeholder="Denver"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Email
+                  </label>
+                  <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="text"
+                    placeholder="john@gmail.com"
+                  />
+                  <p className="text-red-500 text-xs italic">
+                    Please enter email.
+                  </p>
+                </div>
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Message
+                  </label>
+                  <textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                  <p className="text-red-500 text-xs italic">
+                    Enter description
+                  </p>
+                </div>
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Resume
+                  </label>
+                  <input
+                    onChange={(e) => {
+                      if (e.target.files?.length) {
+                        setResume(e.target.files[0]);
+                      }
+                    }}
+                    accept=".pdf"
+                    className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="resume"
+                    type="file"
+                  />
+                  <p className="text-red-500 text-xs italic">
+                    Please attach resume in pdf.
+                  </p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <button
+                    onClick={handleSubmit}
+                    className="bg-primary hover:bg-accent text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        {alert && <AlertMessage message={message} type={type} closeAlert={()=>showAlert(false)}/>}
-        </>
-    )
+      </div>
+      {alert && (
+        <AlertMessage
+          message={message}
+          type={type}
+          closeAlert={() => showAlert(false)}
+        />
+      )}
+    </>
+  );
 }
 
-export default Contacts
+export default Contacts;
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,81 @@
-import Navbar from '@/components/navbar/navbar'
-import WorkBanner from '@/components/workBanner/workbanner'
-import Image from 'next/image'
-import {Anton} from 'next/font/google'
-import Events from './model/events.modle'
-import { connect } from './config/db.config'
-import  EventComponent from '@/components/events/events'
-
+import Navbar from "@/components/navbar/navbar";
+import WorkBanner from "@/components/workBanner/workbanner";
+import Image from "next/image";
+import { Anton } from "next/font/google";
+import Events from "./model/events.modle";
+import { connect } from "./config/db.config";
+import EventComponent from "@/components/events/events";
 
 const anton = Anton({
-  subsets: ['latin'],
-  weight: '400',
-  variable:'--anton-font'
-})
-
-
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--anton-font",
+});
 
 async function Page() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/events?page=1`,{ cache: 'no-store' });
-  const value = await data.json()
+  const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/events?page=1`, {
+    cache: "no-store",
+  });
+  const value = await data.json();
   return (
-   <div className={`parent scroll-smooth flex w-full flex-col items-center md:max-w-5xl`}>
+    <div
+      className={`parent scroll-smooth flex w-full flex-col items-center md:max-w-5xl`}
+    >
       <div className="flex relative flex-col items-center justify-center md:h-[calc(100vh-7rem)]">
         <div className="picAndText flex items-center w-full flex-col">
-          
           <div className="intro mt-2 md:mt-2 flex flex-col-reverse md:flex-row justify-center items-center relative">
             <div className="statement flex-col p-4 md:w-2/3 text-xl text-justify">
               <div>Hi it&apos;s</div>
               <div className="text-4xl md:text-8xl mt-2">Ranjith Mathew</div>
               <p className="md:text-lg mt-7 animate-fade-up animate-infinite">
-                A dedicated community builder and career management professional excels in fostering connections and providing expert guidance in job development, resume writing, and career mentorship. Their focus extends to supporting diverse individuals, including immigrants, graduates, and job seekers, through various initiatives. Active involvement in youth leadership programs and serving on nonprofit boards showcases a commitment to shaping the leaders of tomorrow. Outside of professional commitments, solace is found in fishing, whether casting a line in a local river or venturing to serene spots, allowing for a reconnection with nature and moments of tranquility amidst life&apos; s busyness.
+                A dedicated community builder and career management professional
+                who excels in fostering connections and providing expert
+                guidance in job development, resume writing, and career
+                mentorship. With close to three decades of experience in
+                corporate sales, business development, and resource management,
+                Ranjith brings a wealth of knowledge to his professional
+                endeavors.{" "}
               </p>
+              <p className="md:text-lg mt-7 animate-fade-up animate-infinite">
+                As a Past President of Junior Chamber International – JCI
+                Ottawa, Ranjith focuses on supporting diverse individuals,
+                including new immigrants, recent graduates, and job seekers,
+                through various initiatives. He is actively involved in youth
+                leadership programs and serves on various nonprofit
+                organizations as a mentor and board member, showcasing his
+                commitment to shaping the leaders of tomorrow.
+              </p>
+              {/* <p className="md:text-lg mt-7 animate-fade-up animate-infinite">
+                Throughout his career, Ranjith has assisted over 1,000 new
+                graduates and newcomers in Ottawa in finding job opportunities
+                in the business and IT security industries. His proficiency in
+                community outreach and stakeholder development enables him to
+                build new collaborations, driving solutions for nonprofits and
+                business leaders alike. Ranjith wishes to be your #networKING,
+                passionately connecting people in a world where uncompromised
+                digital trust is essential.
+              </p> */}
             </div>
             <div className="dpimage w-full md:w-auto flex flex-row-reverse mr-10 md:ml-4 md:flex-col md:justify-center md:relative md:mt-12 lg:mt-2 lg:h-[30rem]">
-            <img
-              src="/rm-profile.jpg"
-              alt="pic"
-              className="rounded-full rounded-bl-none h-60 max-w-full md:rounded-none md:h-full"
-            />
-          </div>
+              <img
+                src="/rm-profile.jpg"
+                alt="pic"
+                className="rounded-full rounded-bl-none h-60 max-w-full md:rounded-none md:h-full"
+              />
+            </div>
           </div>
         </div>
         <span className="absolute left-0 bottom-0 hidden md:flex h-10 w-10 animate-bounce">
-           <a href="#work_banner"><img src="/arrow.png" className="rotate-90 h-10 w-10 rounded-full text-background text-center text-4xl"/></a> 
+          <a href="#work_banner">
+            <img
+              src="/arrow.png"
+              className="rotate-90 h-10 w-10 rounded-full text-background text-center text-4xl"
+            />
+          </a>
         </span>
       </div>
-      <WorkBanner/>
-      <EventComponent events={value.events}/>
+      <WorkBanner />
+      <EventComponent events={value.events} />
     </div>
   );
 }
