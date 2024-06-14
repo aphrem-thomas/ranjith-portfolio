471d7d36397b8d678c1a54d4f3aa22676232ea0f
aphremthomas <aphremthomas@gmail.com>
2024-06-14 00:46:33 -0400

    rearranged posts

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ async function Page() {
         </span>
       </div>
       <WorkBanner />
-      {value.events.lenght && <EventComponent events={value.events} />}
+      {value.events.length && <EventComponent events={value.events} />}
     </div>
   );
 }
