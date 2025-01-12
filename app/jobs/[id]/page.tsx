073549afd8f3759724e32e4fde628fd73ec28c67
aphremthomas <aphremthomas@gmail.com>
2025-01-11 19:56:17 -0500

    setting url in code

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -7,7 +7,7 @@ import Jobs from "@/app/model/jobs.model";
 
 
 async function Page({ params }: { params: { id: string } }) {
-  const jobList = await fetch(`http://ranjithmathew.com/api/jobs?page=${params.id}`,{ cache: 'no-store' });
+  const jobList = await fetch(`https://ranjithmathew.com/api/jobs?page=${params.id}`,{ cache: 'no-store' });
   const data = await jobList.json()
   const getLinks = (id:string)=>{
     let links=[];
